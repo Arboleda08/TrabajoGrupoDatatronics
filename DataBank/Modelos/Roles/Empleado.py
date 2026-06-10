00ffcf11d74ba2ab1f7634aa185bc48c256ea8a6
Sofia Valentina Arboleda Beltran <soarboledab@unal.edu.co>
2026-06-10 18:47:41 -0500

    Update Empleado.py

--- DataBank/Modelos/Roles/Empleado.py
+++ DataBank/Modelos/Roles/Empleado.py
@@ -1,65 +1,81 @@
 class Empleado:
-    total_employees: int = 0
+    total_empleados: int = 0
     
-    def __init__(self, name: str, dni: int, position: str, salary: float, experience: int) -> None:
-        Empleado.total_employees +=1
-        self.name = name
-        self.__position = position
+    def __init__(self, nombre: str, dni: int, posicion: str, salario: float, experiencia: int) -> None:
+        Empleado.total_empleados +=1
+        self.nombre = nombre
+        self.__posicion = posicion
         self.__dni = dni
-        self.__salary = salary
-        self.experience = experience
-        self.is_blocked = False
-        self.failed_attempts = 0
+        self.__salario = salario
+        self.experiencia = experiencia
+        self.esta_bloqueado = False
+        self.intentos_fallidos = 0
+        self.puede_cambiar_role = False
+        self.bloqueado_hasta = None
 
-    def get_position(self):
-        return (self.__position)
+    def conseguir_posicion(self):
+        return (self.__posicion)
     
-    def set_position(self, new_position):
-        self.__position = new_position
+    def establecer_posicion(self, nueva_posicion):
+        self.__posicion = nueva_posicion
 
-    def get_dni(self):
+    def conseguir_dni(self):
         return (self.__dni)
     
-    def get_salary(self):
-        return (self.__salary)
+    def conseguir_salario(self):
+        return (self.__salario)
     
-    def set_salary(self, new_salary):
-        self.__salary = new_salary
+    def establecer_salario(self, nuevo_salario):
+        self.__salario = nuevo_salario
     
-    def obtain_bonus(self) -> float:
+    def obtener_bonus(self) -> float:
         raise NotImplementedError("El método debe ser implementado en la sublclase.")
 
-    def raise_salary(self):
-        self.set_salary(
-            self.get_salary() * (1 + self.percentage_increase())
+    def aumentar_salario(self):
+        self.establecer_salario(
+            self.conseguir_salario() * (1 + self.porcentaje_de_incremento())
         )
     
-    def can_approve_credit(self, amount: float)-> bool:
+    def puede_aprobar_creditos(self, monto: float)-> bool:
         return False
     
-    def can_modify_salary(self, employee: "Empleado", amount: float)-> bool:
+    def puede_modificar_salario(self, empleado: "Empleado", monto: float)-> bool:
         return False
     
-    def can_see_reports(self) ->bool:
+    def puede_ver_reportes(self) ->bool:
         return False
     
-    def can_see_information(self) -> bool:
+    def puede_ver_informacion(self) -> bool:
         return False
     
-    def can_approve_transfer(self, amount: float) -> bool:
+    def puede_aprobar_transferencia(self, monto: float) -> bool:
         return False
     
-    def can_create_user(self) -> bool:
+    def puede_crear_usuario(self) -> bool:
         return False
     
-    def can_delete_user(self) ->bool:
+    def puede_eliminar_usuario(self) ->bool:
         return False
     
-    def can_raise_salary(self, employee: "Empleado") -> bool:
+    def puede_aumentar_salario(self, empleado: "Empleado") -> bool:
         return False
     
-    def percentage_increase(self) -> float:
+    def porcentaje_incrementado(self) -> float:
         return 0.01
     
-    def can_request_promotion(self):
+    def puede_solicitar_promocion(self):
         return self.experience >= 5
+    def to_dict(self):
+        return {
+            "tipo_de_empleado": tipo(self).__nombre__,
+            "nombre": self.name,
+            "dni": self.establecer_dni(),
+            "posicion": self.establecer_posicion(),
+            "salario": self.establecer_salario(),
+            "experiencia": self.experiencia,
+            "esta_bloqueado": self.esta_bloqueado,
+            "intentos_fallidos": self.intentos_fallidos,
+            "puede_cambiar_roles": self.puede_cambiar_roles,
+            "bloqueado_hasta": self.bloqueado_hasta.isoformato() if self.bloqueado_hasta else None
+        }
+  
