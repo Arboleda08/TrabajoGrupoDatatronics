3319b7de884df60823b8e7d17f9c40a08e9f567b
Sofia Valentina Arboleda Beltran <soarboledab@unal.edu.co>
2026-06-10 18:59:21 -0500

    Update EmpleadoAutenticable.py

--- DataBank/Modelos/Roles/EmpleadoAutenticable.py
+++ DataBank/Modelos/Roles/EmpleadoAutenticable.py
@@ -1,14 +1,30 @@
+from datetime import datetime
 from Modelos.Roles.Empleado import Empleado
-from Modelos.AutenticableHelper.AutenticableHelper import AutenticableHelper
+from Modelos.AyudanteAuténtico.AyudanteAuténtico import AyudanteAuténtico
 
-class EmpleadoAutenticable(Empleado):
-    def __init__(self, name: str, dni: int, position: str, salary: float, experience: int, password: str):
-        super().__init__(name, dni, position, salary, experience)
-        self._helper = AutenticableHelper()
-        self.password = password
+class AutentificarEmpleado(Empleado):
+    def __init__(self, nombre: str, dni: int, posicion: str, salario: float, experiencio: int, contraseña: str):
+        super().__init__(nombre, dni, posicion, salario, experiencia)
+        self._ayudante = AyudanteAutentico()
+        self.__contraseña = contraseña
+        self.bloquear_hasta = None
 
-    def authenticate_user(self, new_password: str):
-        return self._helper.comparate_passwords(self.password, new_password)
+    def conseguir_contraseña(self):
+        return self.__contraseña
+    
+    def autentificar_usuario(self, nueva_contraseña: str):
+        return self._ayudante.comparar_contraseñas(self.consegir_contraseña(), nueva_contraseña)
 
-    def obtain_bonus(self) -> float:
+    def obtener_bonus(self) -> float:
         return 0
+    
+    def esta_bloqueado(self):
+        if self.bloqueado_hasta is not None and datetime.now() >= self.bloqueado_hasta:
+            self.bloqueado_hasta = None
+            return False
+        return self.bloqueado_hasta is not None
+    
+    def to_dict(self):
+        data = super().to_dict()
+        data["contraseña"] = self.conseguir_contraseña()
+        return data
