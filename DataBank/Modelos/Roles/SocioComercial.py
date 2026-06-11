dc5637055533320b7a8f26584fa32d4ec6a86c17
Sofia Valentina Arboleda Beltran <soarboledab@unal.edu.co>
2026-06-10 19:05:29 -0500

    Refactor SocioComercial to use AyudanteAuténtico
    Updated the authentication helper and added a method to convert the object to a dictionary.

--- DataBank/Modelos/Roles/SocioComercial.py
+++ DataBank/Modelos/Roles/SocioComercial.py
@@ -1,8 +1,13 @@
-from Modelos.AutenticableHelper.AutenticableHelper import AutenticableHelper
+from Modelos.AyudanteAuténtico.AyudanteAuténtico import AyudanteAuténtico
 class SocioComercial:
     def __init__(self) -> None:
-        self._helper = AutenticableHelper()
+        self._ayudante = AyudanteAuténtico()
         self.clave: str | None = None
 
     def autenticar_usuario(self, clave: str) -> bool:
-        return self._helper.comparate_passwords(self.clave, clave)
+        return self._helper.comparar_contraseñas(self.clave, clave)
+    
+    def to_dict(self):
+        return {
+            "clave": self.clave
+        }
