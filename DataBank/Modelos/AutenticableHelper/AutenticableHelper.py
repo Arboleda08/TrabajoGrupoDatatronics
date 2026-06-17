1317e725409c5a4ab2fb95c42c67da5a40fb9a30
Sofia Valentina Arboleda Beltran <soarboledab@unal.edu.co>
2026-06-17 00:11:39 -0500

    Delete DataBank directory
    actualizar cambios

--- DataBank/Modelos/AutenticableHelper/AutenticableHelper.py
+++
@@ -1,3 +0,0 @@
-class AutenticableHelper:
-    def comparar_contraseñas(self, contraseña_guardada: str | None, contraseña_escrita: str) -> bool:
-        return contraseña_guardada == contraseña_escrita
--- DataBank/Modelos/Excepciones/SaldoInsuficienteException.py
+++
@@ -1,2 +0,0 @@
-class SaldoInsuficienteException(Exception):
-    pass
